be51d575bc3807f8452756c21f7afb6b8eb12b75
Mason Wang <wangyachenmason@163.com>
2024-12-20 16:15:33 -0800

    add PlayerContext to handle global audioRef

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'
 import Display from './components/Display'
+import { PlayerContext } from './context/playerContext'
 
 const App = () => {
+
+  const {audioRef} = useContext(PlayerContext);
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
@@ -12,6 +16,7 @@ const App = () => {
       </div>
       
       <Player />
+      <audio ref={audioRef} preload='auto'></audio>
     </div>
   )
 }
--- spotify-clone/src/main.jsx
+++ spotify-clone/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import PlayerContextProvider from './context/playerContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <PlayerContextProvider>
+        <App />
+      </PlayerContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
