0b9f586efa6612ec7a74e806acb51352c6d654e6
Mason Wang <wangyachenmason@163.com>
2024-12-22 17:39:58 -0800

    Add dynamic track selection and playback logic using PlayerContext

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -32,6 +32,12 @@ const PlayerContextProvider = (props) =>{
         setPlayStatus(false);
     }
 
+    const playWithId = async (id) => {
+        await setTrack(songsData[id]);
+        await audioRef.current.play();
+        setPlayStatus(true);
+    }
+
     useEffect(() => {
         setTimeout(() => {
             seekBar.current.style.width = (Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100)) + "%";
@@ -59,7 +65,8 @@ const PlayerContextProvider = (props) =>{
         track, setTrack,
         playStatus, setPlayStatus,
         time, setTime,
-        play, pause
+        play, pause,
+        playWithId
     }
 
     return (
