2e6058b93cc5b4ff00815804497181f3ea52b268
Mason Wang <wangyachenmason@163.com>
2024-12-22 18:00:45 -0800

    Add seekBar feature using PlayerContext

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -53,6 +53,10 @@ const PlayerContextProvider = (props) =>{
                 setPlayStatus(true);
             }
         }
+    
+    const seekSong = async (e) => {
+        audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration)
+    } 
 
     useEffect(() => {
         setTimeout(() => {
@@ -83,7 +87,8 @@ const PlayerContextProvider = (props) =>{
         time, setTime,
         play, pause,
         playWithId,
-        previous, next
+        previous, next,
+        seekSong
     }
 
     return (
