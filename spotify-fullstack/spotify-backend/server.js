fbfcaf27a19987a35f2ae57ce5be342574629037
Mason Wang <wangyachenmason@163.com>
2024-12-23 22:49:31 -0800

    config cloudinary

--- spotify-fullstack/spotify-backend/server.js
+++ spotify-fullstack/spotify-backend/server.js
@@ -2,10 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import songRouter from './src/routes/songRoute.js';
+import connectDB from './src/config/mongodb.js';
 
 // app config 
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+
 
 // middlewares
 app.use(express.json());
