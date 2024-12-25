b5b213bb61a7b08f530ebb5f83f7518cf978d3ca
Mason Wang <wangyachenmason@163.com>
2024-12-24 21:00:02 -0800

    fix MONGODB_URI bug; connection established

--- spotify-fullstack/spotify-backend/src/config/mongodb.js
+++ spotify-fullstack/spotify-backend/src/config/mongodb.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
+console.log("MongoDB URI:", process.env.MONGODB_URI);
+
 const connectDB = async () => {
 
     mongoose.connection.on('connected', () => {
         console.log("connection established")
     });
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`);
 
 }
 
--- spotify-fullstack/spotify-backend/src/controllers/songController.js
+++ spotify-fullstack/spotify-backend/src/controllers/songController.js
@@ -1,5 +1,4 @@
-import { v2 as cloudinary } from 'cloudinary'
-import songModel from '../models/songModel.js';
+
 
 const addSong = async (req, res) => {
 
