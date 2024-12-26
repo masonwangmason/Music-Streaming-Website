3e0a3d7aa7cf8e0bc60238d161ace7db38ff1290
Mason Wang <wangyachenmason@163.com>
2024-12-25 18:15:35 -0800

    add Sidebar/Finish Sidebar design

--- spotify-fullstack/spotify-admin/src/App.jsx
+++ spotify-fullstack/spotify-admin/src/App.jsx
@@ -6,12 +6,13 @@ import AddAlbum from './pages/AddAlbum';
 import ListAlbum from './pages/ListAlbum';
 import AddSong from './pages/AddSong';
 import ListSong from './pages/ListSong';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
     <div className='flex items-start min-h-screen'>
       <ToastContainer />
-
+      <Sidebar />
       <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
 
         <div className="pt-8 pl-5 sm:pt-12 sm:pl-12">
