e113e01a71171427b6f6dd0f2e837c1d5c8f759e
Mason Wang <wangyachenmason@163.com>
2024-12-26 13:25:11 -0800

    Update AddSong Functionality

--- spotify-fullstack/spotify-admin/src/App.jsx
+++ spotify-fullstack/spotify-admin/src/App.jsx
@@ -9,6 +9,8 @@ import ListSong from './pages/ListSong';
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
+export const url = 'http://localhost:4000'
+
 const App = () => {
   return (
     <div className='flex items-start min-h-screen'>
