51b8c0e36c680db087f51a4eab6b6e63e41e0cb5
Mason Wang <wangyachenmason@163.com>
2025-01-29 11:22:09 -0800

    update .gitignore

--- spotify-fullstack/spotify-clone/src/components/Display.jsx
+++ spotify-fullstack/spotify-clone/src/components/Display.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react'
+import {useEffect, useRef} from 'react'
 import { Route, Routes, useLocation} from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
